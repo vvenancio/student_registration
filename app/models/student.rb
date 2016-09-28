a246a57a1f1e509a22bc4d225a0f9ae5e03c0dbd
Vinicius Venancio <vinicius.venancio@live.com>
2016-09-28 10:14:57 -0300

    feature: implements enumeration on student status

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 45 }
   validates :register_number, presence: true, length: { maximum: 45 }
+
+  has_enumeration_for :status, required: true
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe Student, type: :model do
     it { is_expected.to validate_length_of(:name).is_at_most(45) }
     it { is_expected.to validate_presence_of(:register_number) }
     it { is_expected.to validate_length_of(:register_number).is_at_most(45) }
+    it { is_expected.to validate_presence_of(:status) }
   end
 end
